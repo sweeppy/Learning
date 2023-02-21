3d3b11c0f9215b446283b3e8404a92384ca48efa
sweeppy <vikonotopoff@gmail.com>
2023-02-21 18:14:37 +0300

    task1
    Что нужно сделать
    Пользователь вводит в консольном приложении длинное предложение. Каждое слово в этом предложении отделено пробелом. Создайте метод, который в качестве входного параметра принимает строковую переменную, а в качестве возвращаемого значения — массив слов. После вызова этого метода программа вызывает второй метод, который выводит каждое слово в отдельной строке. Метод должен выглядеть так: static string[] SplitText(string text), где text — это строка, которую необходимо разделить.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    /// <summary>
+    /// Разбивает введенную строку на слова
+    /// </summary>
+    /// <param name="str">вводимая строчка пользователем</param>
+    /// <returns>массив слов из строки</returns>
+    static string[] SplitText(string str)
+    {
+        string[] splitStr = str.Split(" ");
+        return splitStr;
+
+    }
+    /// <summary>
+    /// Выводит строковый массив по элементам с новой строки
+    /// </summary>
+    /// <param name="splitedStr">входной строковый массив</param>
+    static void OutputSplitedStr(string[] splitedStr)
+    {
+        foreach (var item in splitedStr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine($"Введите предложение:");
+
+        string takenStr = Console.ReadLine(); /*Ввод предложения пользователем*/
+
+        OutputSplitedStr(SplitText(takenStr)); /*выполнение методов*/
+
+        Console.WriteLine();
+        Console.ReadKey();
+
+    }
+
+}
