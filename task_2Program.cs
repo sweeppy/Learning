39d9f5fa460715d9f1b040c9e99dcf1b5ffa17e9
sweeppy <vikonotopoff@gmail.com>
2023-02-21 18:41:50 +0300

    task_2
    Задание 2. Перестановка слов в предложении
    Что нужно сделать
    Пользователь вводит в программе длинное предложение. Каждое слово отделено пробелом. После ввода пользователь нажимает клавишу Enter. Необходимо создать два метода:
    
    первый разделяет слова в предложении;
    второй меняет эти слова местами (в обратной последовательности).

---
+++ task_2Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    /// <summary>
+    /// Разбивает введенную строку на слова(строковый массив)
+    /// </summary>
+    /// <param name="str">строка</param>
+    /// <returns>строковый массив</returns>
+    static string[] SplitText(string str)
+    {
+        string[] splitStr = str.Split(" ");
+        return splitStr;
+
+    }
+    /// <summary>
+    /// Выводит строковый массив по элементам с новой строки в обратном порядке
+    /// </summary>
+    /// <param name="splitedStr">строковый массив</param>
+    static void SentenceRevers(string[] splitedStr)
+    {
+        for (int i = splitedStr.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(splitedStr[i]);
+        }
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine($"Введите предложение:");
+
+        string takenStr = Console.ReadLine(); /*Ввод предложения пользователем*/
+
+        SentenceRevers((SplitText(takenStr))); /*выполнение методов*/
+
+        Console.WriteLine();
+        Console.ReadKey();
+
+    }
+
+}
