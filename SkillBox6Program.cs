321f3e26038207ab223615a9f9bd30a29cbaaf4d
sweeppy <vikonotopoff@gmail.com>
2023-03-02 18:42:27 +0300

    SkillBox6
    Что нужно сделать
    Создайте справочник «Сотрудники».
    
    Разработайте для предполагаемой компании программу, которая будет добавлять записи новых сотрудников в файл. Файл должен содержать следующие данные:
    
    ID
    Дату и время добавления записи
    Ф. И. О.
    Возраст
    Рост
    Дату рождения
    Место рождения
    Для этого необходим ввод данных с клавиатуры. После ввода данных:
    
    если файла не существует, его необходимо создать;
    если файл существует, то необходимо записать данные сотрудника в конец файла.
    При запуске программы должен быть выбор:
    
    введём 1 — вывести данные на экран;
    введём 2 — заполнить данные и добавить новую запись в конец файла.
    
    Файл должен иметь следующую структуру:
    
    1#20.12.2021 00:12#Иванов Иван Иванович#25#176#05.05.1992#город Москва
    2#15.12.2021 03:12#Алексеев Алексей Иванович#24#176#05.11.1980#город Томск
    …

---
+++ SkillBox6Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Solution
+{
+    static void CheckReadLine(bool condition)
+    {
+        if (condition == false)
+        {
+
+        }
+    }
+    /// <summary>
+    /// Записывает данный переданные пользователем в файл
+    /// </summary>
+    static void WritingInfo() 
+    {
+        string [] lines = File.ReadAllLines(@"e:\workersFile.txt");
+        int count = 1; /*ID клиента*/
+        if (lines.Length == 0)
+            count = 1;
+        else
+            count = lines.Length + 1;
+        string[] information = new string[7];
+        Console.WriteLine($"Введите своё Ф. И. О.:");
+        string fullName = Console.ReadLine();
+        Console.WriteLine($"Введите свой возраст:");
+        bool successOfAge = int.TryParse(Console.ReadLine(), out var age);
+        while (successOfAge == false || age > 80 || age < 16)
+        {
+            Console.WriteLine($"Введено некоректное число, повторите ввод:");
+            successOfAge = int.TryParse(Console.ReadLine(), out age);
+        }
+        Console.WriteLine($"Введите свой рост");
+        bool successOfHeight = int.TryParse(Console.ReadLine(), out var height);
+        while (successOfAge == false || height > 230 || height < 130)
+        {
+            Console.WriteLine($"Введено некоректное число, повторите ввод:");
+            successOfHeight = int.TryParse(Console.ReadLine(), out height);
+        }
+        Console.WriteLine($"Введите свою дату рождения: \n формат записи:##.##.####");
+        string dateOfBirthday = Console.ReadLine();
+        Console.WriteLine($"Введите место рождения:");
+        string placeOfBirthday = Console.ReadLine();
+        FileInfo fileInfo = new FileInfo(@"e:\workersFile.txt");
+
+        information[0] = Convert.ToString(count);             /*ID клиента*/
+        information[1] = fileInfo.LastWriteTime.ToString();   /*Время добавлеия информации*/
+        information[2] = fullName;                            /*Ф.И.О. работника*/
+        information[3] = age.ToString();                      /*Возраст работника*/
+        information[4] = height.ToString();                   /*Росто работника*/
+        information[5] = dateOfBirthday;                      /*Дата рождения работника*/
+        information[6] = $"город {placeOfBirthday}";          /*Место рождения работника*/
+        using (StreamWriter sw = new StreamWriter(@"e:\workersFile.txt", true))
+        {
+            foreach (var item in information)
+            {
+                sw.Write($"#{item}");
+            }
+            sw.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    static void Main()
+    {
+        
+
+        Console.Write($"Введите 1: для того чтобы просмортреть файл \n        2: для того чтобы заполнить данные и добавить новую запись в конец файла.");
+        Console.WriteLine();
+        bool success = int.TryParse(Console.ReadLine(), out var result);
+        while (success == false || result > 2)
+        {
+            Console.WriteLine($"Введено некоректное число, повторите ввод:");
+            success = int.TryParse(Console.ReadLine(), out result);
+        }
+        
+        
+            if(File.Exists(@"e:\workersFile.txt")) /*Проверка существования нужного файла*/
+        {
+                switch (result)
+                {
+                    case 1:
+
+                        string consoleInf;
+                        consoleInf = File.ReadAllText(@"e:\workersFile.txt"); /*Вывод в консоль данных с нужного файла*/
+                        Console.WriteLine(consoleInf.Replace("#", " "));
+                        break;
+
+                    case 2:
+
+                        WritingInfo();
+                        
+                        break;
+
+                }
+            }
+            else
+            {
+            string fileName = @"e:\workersFile.txt";
+            using (FileStream fs = File.Create(fileName));
+            Console.WriteLine("Так как файла не было на вашем компьютере, он был успешно создан \n Путь к файлу: Е:/workersFile");
+            Console.WriteLine();
+            Main();
+            }
+            
+        
+
+
+        Console.ReadKey();
+    }
+    
+}
